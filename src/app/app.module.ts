a2e1ff83942ab7abe94f14cf79bdac47e0ea4eec
Fosso Arcel <kraulain@gmail.com>
2018-09-12 11:58:50 +0100

    Fosso, Debenick, Thierry | create dashboard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { SideBarComponent } from './side-bar/side-bar.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { FooterBarComponent } from './footer-bar/footer-bar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SideBarComponent,
     TopBarComponent,
-    FooterBarComponent
+    FooterBarComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule
